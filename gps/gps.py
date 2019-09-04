6a5b29e3e5536ca24b48fa9de29dd6174ba8bdc6
mehmet ozdemir <mehmetozdemir206@gmail.com>
2019-09-04 14:57:52 +0300

    Update gps.py

--- gps/gps.py
+++ gps/gps.py
@@ -6,3 +6,16 @@ geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
 geo_request = requests.get(geo_request_url)
 geo_data = geo_request.json()
 print({'organization': geo_data['organization']})
+
+# {
+# "area_code": "0",
+# "continent_code": "NA",
+# "country": "United States",
+# "country_code": "US",
+# "country_code3": "USA",
+# "ip": "198.975.33.4",
+# "latitude": "37.7510",
+# "longitude": "-97.8220",
+# "organization": "AS15169 Google Inc.",
+# "timezone": ""
+# }
