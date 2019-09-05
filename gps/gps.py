4a7eab9a299a4254383493568fd24656e2030780
mehmet ozdemir <mehmetozdemir206@gmail.com>
2019-09-05 09:34:10 +0300

    Update gps.py

--- gps/gps.py
+++ gps/gps.py
@@ -1,11 +1,16 @@
 import requests
+#kullanicinin genel ip adresini bulun 
+#Burada kullanacağım GeoIP API 'https://geojs.io/' dır, ancak benzer JSON verisi sunan herhangi bir hizmet işe yarayacaktır.
 ip_request = requests.get('https://get.geojs.io/v1/ip.json')
 my_ip = ip_request.json()['ip']  #ip_request.json() => {ip: '127.0.0.1'}
-print(my_ip)
-geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
+print(my_ip) #ip adresinizi yazdirmak icin
+geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json' 
 geo_request = requests.get(geo_request_url)
 geo_data = geo_request.json()
-print({'organization': geo_data['organization']})
+print(geo_data)]#tum bilgiyi almak icin yazdir
+
+#spesifik bilgileer icin
+#{'organization': geo_data['organization']} seklinde yazdir
 
 # {
 # "area_code": "0",
